63b583a679db5393f50818bac8151164c4718c77
Anik66 <anikkumardas966@gmail.com>
2024-05-15 15:45:28 +0600

    add done

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const client = new MongoClient(uri, {
 
      
       
-      // Send a ping to confirm a successful connection
+      
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
