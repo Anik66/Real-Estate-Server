e9b1e509894a0bea1704958caa8ca018fbc4253c
Anik66 <anikkumardas966@gmail.com>
2024-05-15 15:46:27 +0600

    comment cut

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0wrhevo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
